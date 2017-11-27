1997937a9b1aaefaf79f24b8d0da9bfc8efd5f28
Adrianne Alves <driah_as@hotmail.com>
2017-11-27 20:19:08 -0300

    Initial structure

---
+++ lispf_ck-interpreter.py
@@ -0,0 +1,91 @@
+import ox
+import click
+
+#making lexer
+
+lexer = ox.make_lexer([        
+    ('NUMBER', r'\d+'),
+    ('NAME', r'[-a-zA-Z]+'),
+    ('COMMENT', r';.*'),
+    ('NEWLINE', r'\n'),
+    ('SPACE', r'\s+'),
+    ('RIGHT', r'right'),
+    ('LEFT', r'left'),
+    ('INC', r'inc'),
+    ('DEC', r'dec'),
+    ('ADD',r'add'),
+    ('SUB',r'sub'),
+    ('PRINT', r'print'),
+    ('READ', r'read'),
+    ('DO',r'do'),
+    ('LOOP', r'loop'),
+    ('DEF', r'def'),
+    ('PARENTHESIS_B', r'\('),
+    ('PARENTHESIS_A', r'\)')        
+])
+
+#Seting tokens
+tokens = ['NUMBER','INC', 'DEC','SUB', 'ADD','RIGHT', 'LEFT','PRINT','DO','NAME','LOOP',
+            'READ','DEF','PARENTHESIS_A','PARENTHESIS_B']
+
+op = lambda op: (op)
+operator = lambda type_op: (type_op)
+
+#making parser
+parser = ox.make_parser([
+    ('program : PARENTHESIS_B expr PARENTHESIS_A', lambda x,y,z: y),
+    ('program : PARENTHESIS_B PARENTHESIS_A', lambda x,y: '()'),
+    ('expr : operator expr', lambda x,y: (x,) + y),
+    ('expr : operator', lambda x: (x,)),
+    ('operator : program', op),
+    ('operator : LOOP', operator),
+    ('operator : DO', operator),
+    ('operator : RIGHT', operator),
+    ('operator : LEFT', operator),
+    ('operator : READ', operator),
+    ('operator : INC', operator),
+    ('operator : DEC', operator),
+    ('operator : DEF', operator),
+    ('operator : PRINT', operator),
+    ('operator : ADD', operator),
+    ('operator : SUB', operator),
+    ('operator : NAME', operator),
+    ('operator : NUMBER', operator),
+], tokens)
+
+#import operator as op
+#operations = {'inc': op.add, 'dec': op.sub}
+
+#def eval(ast):
+ #   head, *tail = ast
+  #  if head == 'atom':
+   #     return tail[0]
+    #else:
+     #   func = operations[head]
+      #  args = (eval(x) for x in tail)
+       # return func(*args)
+
+
+
+#enter the file
+@click.command()
+@click.argument('source', type=click.File('r'))
+
+def eval_loop(source):
+    program = source.read()
+
+    tokens = lexer(program)
+
+     #removing comments and spaces of tokens to make the tree
+    parser_tokens = [token for token in tokens if token.type != 'COMMENT' and token.type != 'SPACE']
+
+    tree = parser(parser_tokens)
+
+    print(tree)
+
+    #value = eval(tree)
+
+    #print('result:', value)
+
+if __name__ == '__main__':
+   eval_loop()
